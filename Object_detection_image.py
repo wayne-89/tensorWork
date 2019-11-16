7451a4b3457c3af734bd127463afa204730e5259
wayne <pangweizju@163.com>
2019-11-16 17:33:27 +0800

    :hammer:

--- Object_detection_image.py
+++ Object_detection_image.py
@@ -69,8 +69,8 @@ else:
 
 if sys.argv[5] is not None:
     # labelNameMap=ast.literal_eval(sys.argv[5])
-    labelNameMap=json.loads(sys.argv[5])
-    print('labelNameMapStr labelNameMap',labelNameMap)
+    labelNameMap = sys.argv[5]
+    # print('labelNameMapStr labelNameMap', labelNameMap)
 # Load the label map.
 # Label maps map indices to category names, so that when our convolution
 # network predicts `5`, we know that this corresponds to `king`.
