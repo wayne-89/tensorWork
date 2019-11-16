70e57753b4c56a9b8eaf4015598af371e0ea33a9
wayne <pangweizju@163.com>
2019-11-16 16:55:15 +0800

    :hammer:

--- utils/visualization_utils.py
+++ utils/visualization_utils.py
@@ -200,15 +200,15 @@ def draw_bounding_box_on_image(image,
   draw.line([(left, top), (left, bottom), (right, bottom),
              (right, top), (left, top)], width=thickness, fill=color)
   try:
-    # font = ImageFont.truetype('arial.ttf', 24)
-    font = ImageFont.load_default()
+    font = ImageFont.truetype('arialuni.ttf', 24)
+    # font = ImageFont.load_default()
   except IOError:
     font = ImageFont.load_default()
 
   # If the total height of the display strings added to the top of the bounding
   # box exceeds the top of the image, stack the strings below the bounding box
   # instead of above.
-  print('吴莎莎哈哈哈哈哈',display_str_list,font.getsize('aaaa'))
+  print('吴莎莎哈哈哈哈哈',display_str_list,font.getsize('哈'))
   display_str_heights = [11 for ds in display_str_list]
   # Each display_str has a top and bottom margin of 0.05x.
   total_display_str_height = (1 + 2 * 0.05) * sum(display_str_heights)
