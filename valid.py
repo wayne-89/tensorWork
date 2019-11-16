298c81192c510802020af640998d942ff0f5af50
wayne <pangweizju@163.com>
2019-11-16 22:21:04 +0800

    :hammer:

--- valid.py
+++ valid.py
@@ -5,34 +5,36 @@ import sys
 import configparser
 import json
 import codecs
+
 # os.system('conda activate tensorflow1')
 # init
 curPath = sys.path[0]
 basePath = curPath
 workPath = curPath
 dstPath = sys.argv[1]
-labelNameMap={}
+labelNameMap = {}
 print(sys.stdin.encoding, sys.stdout.encoding)
 if len(sys.argv) >= 3:
-	dstImage=sys.argv[2]
+    dstImage = sys.argv[2]
 else:
-	dstImage=os.path.join(dstPath,"images/valid")
+    dstImage = os.path.join(dstPath, "images/valid")
 if len(sys.argv) >= 4:
-	labelNameMap = json.dumps(sys.argv[3])
-	print('mmmmm',json.dumps(labelNameMap))
-cfgPath=os.path.join(dstPath,"config.conf")
-labelMapPath=os.path.join(dstPath,"labelmap.pbtxt")    
-baseSection="base"
-labelSection="label"
+    labelNameMap = sys.argv[3]
+    print('mmmmm', labelNameMap)
+cfgPath = os.path.join(dstPath, "config.conf")
+labelMapPath = os.path.join(dstPath, "labelmap.pbtxt")
+baseSection = "base"
+labelSection = "label"
 
 cf = configparser.ConfigParser()
 cf.read(cfgPath)
-numClasses = cf.get(baseSection,"num_classes")
+numClasses = cf.get(baseSection, "num_classes")
 MODEL_NAME = 'inference_graph'
-PATH_TO_CKPT=os.path.join(dstPath,MODEL_NAME,'frozen_inference_graph.pb')
+PATH_TO_CKPT = os.path.join(dstPath, MODEL_NAME, 'frozen_inference_graph.pb')
 
-os.system('python {0}/Object_detection_image.py {1} {2} {3} {4} {5}'.format(basePath,PATH_TO_CKPT,labelMapPath,dstImage,numClasses,labelNameMap))
-
+os.system(
+    'python {0}/Object_detection_image.py {1} {2} {3} {4} {5}'.format(basePath, PATH_TO_CKPT, labelMapPath, dstImage,
+                                                                      numClasses, labelNameMap))
 
 # if os.path.isfile(dstImage):
 #     os.system('python {0}/Object_detection_image.py {1} {2} {3} {4}'.format(basePath,PATH_TO_CKPT,labelMapPath,dstImage,numClasses))
@@ -47,9 +49,3 @@ os.system('python {0}/Object_detection_image.py {1} {2} {3} {4} {5}'.format(base
 #             filepath = os.path.join(dstImage,filename)
 #             cnt=cnt-1
 #             os.system('python {0}/Object_detection_image.py {1} {2} {3} {4} {5}'.format(basePath,PATH_TO_CKPT,labelMapPath,filepath,numClasses,cnt==0))
-
-
-
-
-
-
