e953f9eb98975612e9aacb7f99f77c928d0d6bb9
wayne <pangweizju@163.com>
2019-11-16 21:09:01 +0800

    :hammer:

--- Object_detection_image.py
+++ Object_detection_image.py
@@ -70,8 +70,8 @@ else:
 if sys.argv[5] is not None:
     # labelNameMap=ast.literal_eval(sys.argv[5])
     # loaded = (u' ' + sys.argv[5]).strip()
-    loaded=u'{"5db945f491a663eb188e82ce":"\xe4","5db945c891a663eb188e82cb":"螺母A","5db945d991a663eb188e82cc":"螺丝B","5db945c391a663eb188e82ca":"螺丝A"}'
-    # loaded=u"{\"5db945f491a663eb188e82ce\":\"\udce8\udc9e\udcba\udce6\udcaf\udc8dB\",\"5db945c891a663eb188e82cb\":\"\udce8\udc9e\udcba\udce6\udcaf\udc8dA\",\"5db945d991a663eb188e82cc\":\"\udce8\udc9e\udcba\udce4\udcb8\udc9dB\",\"5db945c391a663eb188e82ca\":\"\udce8\udc9e\udcba\udce4\udcb8\udc9dA\"}"
+    # loaded=u'{"5db945f491a663eb188e82ce":"\xe4","5db945c891a663eb188e82cb":"螺母A","5db945d991a663eb188e82cc":"螺丝B","5db945c391a663eb188e82ca":"螺丝A"}'
+    loaded="{\"5db945f491a663eb188e82ce\":\"\xe4\",\"5db945c891a663eb188e82cb\":\"\udce8\udc9e\udcba\udce6\udcaf\udc8dA\",\"5db945d991a663eb188e82cc\":\"\udce8\udc9e\udcba\udce4\udcb8\udc9dB\",\"5db945c391a663eb188e82ca\":\"\udce8\udc9e\udcba\udce4\udcb8\udc9dA\"}"
     # print('loaddddded',loaded)
     loaded = json.loads(loaded)
     # for key in loaded:
