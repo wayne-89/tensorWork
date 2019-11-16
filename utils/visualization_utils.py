f8a1cc5564aee099977ab4b29683849bec2d1311
wayne <pangweizju@163.com>
2019-11-16 16:30:39 +0800

    :hammer:

--- utils/visualization_utils.py
+++ utils/visualization_utils.py
@@ -208,7 +208,7 @@ def draw_bounding_box_on_image(image,
   # box exceeds the top of the image, stack the strings below the bounding box
   # instead of above.
   print('吴莎莎哈哈哈哈哈',display_str_list,font.getsize('aaaa'))
-  display_str_heights = [font.getsize('aaaa')[1] for ds in display_str_list]
+  display_str_heights = [11 for ds in display_str_list]
   # Each display_str has a top and bottom margin of 0.05x.
   total_display_str_height = (1 + 2 * 0.05) * sum(display_str_heights)
 
@@ -218,7 +218,8 @@ def draw_bounding_box_on_image(image,
     text_bottom = bottom + total_display_str_height
   # Reverse list and print from bottom to top.
   for display_str in display_str_list[::-1]:
-    text_width, text_height = font.getsize(display_str)
+    text_width, text_height = len(display_str)*6
+    # text_width, text_height = font.getsize(display_str)
     margin = np.ceil(0.05 * text_height)
     draw.rectangle(
         [(left, text_bottom - text_height - 2 * margin), (left + text_width,
