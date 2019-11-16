d0ef39e9344baa57dde96c4950b771d882ae6f0a
wayne <pangweizju@163.com>
2019-11-16 18:04:56 +0800

    :hammer:

--- Object_detection_image.py
+++ Object_detection_image.py
@@ -68,8 +68,8 @@ else:
 
 if sys.argv[5] is not None:
     # labelNameMap=ast.literal_eval(sys.argv[5])
-    loaded = json.loads(sys.argv[5])
-    labelNameMap = loaded
+    # loaded = json.loads(sys.argv[5])
+    labelNameMap = sys.argv[5]
     # print('labelNameMapStr labelNameMap', labelNameMap)
 # Load the label map.
 # Label maps map indices to category names, so that when our convolution
--- valid.py
+++ valid.py
@@ -18,9 +18,8 @@ if len(sys.argv) >= 3:
 else:
 	dstImage=os.path.join(dstPath,"images/valid")
 if len(sys.argv) >= 4:
-	labelNameMap=sys.argv[3]
+	labelNameMap = json.dumps(sys.argv[3])
 	print('mmmmm',labelNameMap)
-	# labelNameMap = json.dumps(sys.argv[3])
 cfgPath=os.path.join(dstPath,"config.conf")
 labelMapPath=os.path.join(dstPath,"labelmap.pbtxt")    
 baseSection="base"
