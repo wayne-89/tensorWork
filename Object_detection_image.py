1abcdaa4c9f8fd9c4e91bba505c75faafe03f12b
wayne <pangweizju@163.com>
2019-11-24 16:42:00 +0800

    :hammer:

--- Object_detection_image.py
+++ Object_detection_image.py
@@ -148,7 +148,7 @@ for IMAGE_PATH in IMAGE_PATHS:
         category_index,
         use_normalized_coordinates=True,
         line_thickness=8,
-        min_score_thresh=0.60)
+        min_score_thresh=0.40)
     print('v_res 识别数量: ', len(v_res))
     # All the results have been drawn on image. Now display the image.
     FULL_NAME = IMAGE_PATH.split("/")
